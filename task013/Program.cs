58f5cb3f5dd312b1fa32b176e1c44948338ebeb9
Uladzislau <juniors.fryt@gmail.com>
2023-03-01 01:35:58 +0300

    task013 change

--- task013/Program.cs
+++ task013/Program.cs
@@ -3,7 +3,7 @@ int number = Convert.ToInt32(Console.ReadLine());
 string FiveDigitNumber = Convert.ToString(number);
 if (number >= 10000 && number <= 99999)
 {
-  Console.WriteLine("третья цифра  " + FiveDigitNumber[2]);
+  Console.WriteLine("третья цифра : " + FiveDigitNumber[2]);
 }
 else 
 {

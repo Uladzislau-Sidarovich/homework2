ad208628f31e4f065725979506896ecd59dfbb1b
Uladzislau <juniors.fryt@gmail.com>
2023-03-01 01:30:53 +0300

    added .gitignore and task

---
+++ task010/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введи трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string threeNumber = Convert.ToString(number);
+if (number >= 100 && number <= 1000)
+{
+    Console.WriteLine("вторая цифра этого числа " + threeNumber[1]);
+}
---
+++ task013/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string FiveDigitNumber = Convert.ToString(number);
+if (number >= 10000 && number <= 99999)
+{
+  Console.WriteLine("третья цифра  " + FiveDigitNumber[2]);
+}
+else 
+{
+  Console.WriteLine("третьей цифры нет");
+  Console.WriteLine("это не пятизначное число");
+}
